7154570e01a472e65f45bb9399c59e47f73c1c3b
Emanuella Abygail <abygailsiahaan1911@gmail.com>
2024-09-11 14:26:38 +0700

    yeah

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-oumditmi@_)jlkde6t5pgx0#u2#g*mp0os7z^si_&oj3!ivwt5
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "emanuella-abygail-mntlhlthtrckr.pbp.cs.ui.ac.id"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://emanuella-abygail-mntlhlthtrckr.pbp.cs.ui.ac.id", "https://emanuella-abygail-mntlhlthtrckr.pbp.cs.ui.ac.id"]
 
 
 # Application definition
@@ -123,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://emanuella-abygail-mntlhlthtrckr.pbp.cs.ui.ac.id", "https://emanuella-abygail-mntlhlthtrckr.pbp.cs.ui.ac.id"]
